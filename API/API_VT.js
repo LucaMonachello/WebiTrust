da94cd28df2fc0a881aeecf8b21ef849e4ae70c6
hell-noa <118801845+hell-noa@users.noreply.github.com>
2026-01-08 16:37:23 +0100

    Update API_VT.js
    modifiction par export function

--- API/API_VT.js
+++ API/API_VT.js
@@ -1,5 +1,5 @@
-// API_VT.js
-const fetch = require("node-fetch"); // npm install node-fetch@2
+// API_VT.js (ES module)
+import fetch from "node-fetch"; // npm install node-fetch@2
 
 // ===============================
 // CONFIG
@@ -10,7 +10,7 @@ const VT_API_KEY = "3c96eb427a670f6aa49937d9d6652a349efdc8f793ae0d816ccc2b46cafb
 // FONCTIONS INTERNES
 // ===============================
 
-function encodeUrl(url) {
+export function encodeUrl(url) {
   return Buffer.from(url)
     .toString("base64")
     .replace(/=/g, "")
@@ -18,7 +18,7 @@ function encodeUrl(url) {
     .replace(/\//g, "_");
 }
 
-async function getScores(id) {
+export async function getScores(id) {
   const res = await fetch(`https://www.virustotal.com/api/v3/urls/${id}`, {
     headers: { "x-apikey": VT_API_KEY }
   });
@@ -45,19 +45,8 @@ async function getScores(id) {
 // FONCTION MAIN APPELÉE PAR L’APPLI
 // ===============================
 
-async function main(url) {
+export async function main(url) {
   const id = encodeUrl(url);
   const { vtScore, reputation } = await getScores(id);
-  // on ne renvoie QUE ces deux champs
   return { vtScore, reputation };
 }
-
-// ===============================
-// EXPORT
-// ===============================
-
-module.exports = {
-  main,
-  encodeUrl,
-  getScores
-};
